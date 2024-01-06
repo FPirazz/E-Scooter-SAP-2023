b08f0502c33fc9eb4ca538e89d27828d8536bdf2
Lorenzo Massone <lorenzomassone@gmail.com>
2024-01-06 16:26:01 +0100

    Basic GET request management to "/"

--- Assignment5/Massone/E-Scooter Microservices/build.gradle.kts
+++ Assignment5/Massone/E-Scooter Microservices/build.gradle.kts
@@ -21,7 +21,6 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
---
+++ Assignment5/Massone/E-Scooter Microservices/src/main/kotlin/com/escooter_microservices/escooter/Controller.kt
@@ -0,0 +1,12 @@
+package com.escooter_microservices.escooter
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class Controller {
+    @GetMapping("/")
+    fun home(): String {
+        return "Welcome to the E-Scooter Microservices!"
+    }
+}
