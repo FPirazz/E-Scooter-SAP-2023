918d12987b2699159253984469a61962cd07225c
Lorenzo Massone <lorenzomassone@gmail.com>
2024-01-07 11:28:48 +0100

    Fixed project structure

--- Assignment5/Massone/E-Scooter Microservices/dashboard_service/src/main/kotlin/com/escooter_microservices/escooter/DashboardServiceApplication.kt
+++ Assignment5/Massone/E-Scooter Microservices/dashboard_service/src/main/kotlin/com/escooter_microservices/escooter/DashboardServiceApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class EscooterApplication
+class DashboardServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<EscooterApplication>(*args)
+    runApplication<DashboardServiceApplication>(*args)
 }
--- Assignment5/Massone/E-Scooter Microservices/dashboard_service/src/test/kotlin/com/escooter_microservices/escooter/DashboardServiceApplicationTests.kt
+++ Assignment5/Massone/E-Scooter Microservices/dashboard_service/src/test/kotlin/com/escooter_microservices/escooter/DashboardServiceApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class EscooterApplicationTests {
+class DashboardServiceApplicationTests {
 
 	@Test
 	fun contextLoads() {
