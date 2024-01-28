381ed7345e07e40d494d93afb3bb22281e4cf8bb
Lorenzo Massone <lorenzomassone@gmail.com>
2024-01-28 11:48:25 +0100

    First draft of MaintainanceServiceApp.kt

--- Assignment5/Massone/E-Scooter Microservices/mantainance_service/app/src/main/kotlin/mantainance_service/Controller.kt
+++ Assignment5/Massone/E-Scooter Microservices/mantainance_service/app/src/main/kotlin/mantainance_service/Controller.kt
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.servlet.ModelAndView
 
+/**
+ * Controller for the maintainance service.
+ */
 @Controller
 class Controller {
     @GetMapping("/")
