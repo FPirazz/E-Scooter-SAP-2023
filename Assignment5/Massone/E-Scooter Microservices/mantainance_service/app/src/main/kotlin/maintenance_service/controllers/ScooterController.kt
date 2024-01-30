0e86e51862fe0228fb68ad12051423a80e556d2d
Lorenzo Massone <lorenzomassone@gmail.com>
2024-01-30 17:15:34 +0100

    feat: Enhanced ScooterController with HTML response and updated RidesServiceRoute formatting

--- Assignment5/Massone/E-Scooter Microservices/mantainance_service/app/src/main/kotlin/maintenance_service/controllers/ScooterController.kt
+++ Assignment5/Massone/E-Scooter Microservices/mantainance_service/app/src/main/kotlin/maintenance_service/controllers/ScooterController.kt
@@ -2,21 +2,28 @@ package maintenance_service.controllers
 
 import maintenance_service.models.Scooter
 import maintenance_service.repositories.ScooterRepository
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/create_escooter/")
 class ScooterController(private val scooterRepository: ScooterRepository) {
-
     @PostMapping
-    fun createScooter(@RequestBody scooter: Scooter): ResponseEntity<out Any> {
+    fun createScooter(@RequestBody scooter: Scooter): ResponseEntity<String> {
         if (scooter.name == null || scooter.location == null) {
             return ResponseEntity("Missing request parameters", HttpStatus.BAD_REQUEST)
         }
 
-        val savedScooter = scooterRepository.save(scooter)
-        return ResponseEntity(savedScooter, HttpStatus.CREATED)
+        scooterRepository.save(scooter)
+
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.TEXT_HTML
+        return ResponseEntity("<html><body><p>Escooter created</p></body></html>", headers, HttpStatus.CREATED)
     }
 }
