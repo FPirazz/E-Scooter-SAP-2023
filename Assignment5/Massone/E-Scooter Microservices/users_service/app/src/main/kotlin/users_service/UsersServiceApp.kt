0b80b00e27f7b2185ed77c76c931f838533d9efe
Lorenzo Massone <lorenzomassone@gmail.com>
2024-02-07 18:31:10 +0100

    feat: added Circuit Breaker pattern to UsersServiceVerticle.kt

--- Assignment5/Massone/E-Scooter Microservices/users_service/app/src/main/kotlin/users_service/UsersServiceApp.kt
+++ Assignment5/Massone/E-Scooter Microservices/users_service/app/src/main/kotlin/users_service/UsersServiceApp.kt
@@ -2,7 +2,14 @@ package users_service
 
 import io.vertx.core.Vertx
 
+class UserServiceApp {
+    fun run() {
+        val vertx = Vertx.vertx()
+        vertx.deployVerticle(UsersServiceVerticle())
+    }
+}
+
 fun main() {
-    val vertx = Vertx.vertx()
-    vertx.deployVerticle(UsersServiceVerticle())
+    val app = UserServiceApp()
+    app.run()
 }
