9c988a4a399a636db07e3ae0ce4d61ef5a0f27ed
Lorenzo Massone <lorenzomassone@gmail.com>
2024-02-09 19:00:21 +0100

    refactor: refactored business logic regarding Scooter class in ScooterService.kt

--- Assignment5/Massone/E-Scooter Microservices/mantainance_service/app/src/main/kotlin/maintenance_service/controllers/ScooterController.kt
+++ Assignment5/Massone/E-Scooter Microservices/mantainance_service/app/src/main/kotlin/maintenance_service/controllers/ScooterController.kt
@@ -1,7 +1,7 @@
 package maintenance_service.controllers
 
 import maintenance_service.models.Scooter
-import maintenance_service.repositories.ScooterRepository
+import maintenance_service.services.ScooterService
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -9,18 +9,10 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
 
 @RestController
-class ScooterController(private val scooterRepository: ScooterRepository) {
-    private val logger = LoggerFactory.getLogger(ScooterController::class.java)
-
+class ScooterController(private val scooterService: ScooterService) {
     @PostMapping("/create_escooter/")
-    fun createScooter(@RequestBody scooter: Scooter): ModelAndView {
-        if (scooter.name == null || scooter.location == null) {
-            return ModelAndView("escooter_error")
-        }
-
-        scooterRepository.save(scooter)
-
-        return ModelAndView("escooter_created")
+    fun createScooter(@RequestBody scooter: Scooter): ResponseEntity<Scooter> {
+        return ResponseEntity.ok(scooterService.createScooter(scooter))
     }
 
     @GetMapping("/escooter_created/")
@@ -30,32 +22,24 @@ class ScooterController(private val scooterRepository: ScooterRepository) {
 
     @GetMapping("/all_scooters/")
     fun getAllScooters(): List<Scooter> {
-        return scooterRepository.findAll()
+        return scooterService.getAllScooters()
     }
 
     data class ScooterStateResponse(val state: String)
 
     @GetMapping("/get_scooter_state/{scooterId}/")
     fun getScooterState(@PathVariable scooterId: String): ResponseEntity<ScooterStateResponse> {
-        val scooterOptional = scooterRepository.findById(scooterId)
-        return if (scooterOptional.isPresent) {
-            val scooter = scooterOptional.get()
+        val scooter = scooterService.getScooterState(scooterId)
+        return if (scooter != null) {
             ResponseEntity.ok(ScooterStateResponse(scooter.state ?: "N/A"))
         } else {
             ResponseEntity.status(HttpStatus.NOT_FOUND).body(ScooterStateResponse("error"))
         }
     }
 
-    /**
-     * Handles GET requests to the "/available_scooters/" endpoint. Makes an HTTP GET request to the
-     * "http://localhost:8080/scooters/available_scooters" URL and returns the response as a list of
-     * Scooter objects wrapped in a ResponseEntity.
-     *
-     * @return ResponseEntity containing a list of available Scooter objects.
-     */
     @GetMapping("/available_scooters/")
     private fun getAvailableScooters(): ResponseEntity<List<Scooter>> {
-        val scooters = scooterRepository.findAll().filter { it.state == "ready" }
+        val scooters = scooterService.getAvailableScooters()
         return ResponseEntity.ok(scooters)
     }
 
@@ -64,43 +48,25 @@ class ScooterController(private val scooterRepository: ScooterRepository) {
         @PathVariable scooterId: String,
         @RequestBody updatedScooter: Scooter,
     ): ResponseEntity<String> {
-        val scooterOptional = scooterRepository.findById(scooterId)
-        return if (scooterOptional.isPresent) {
-            val existingScooter = scooterOptional.get()
-            existingScooter.state = updatedScooter.state
-            scooterRepository.save(existingScooter)
+        val scooter = scooterService.setScooterState(scooterId, updatedScooter)
+        return if (scooter != null) {
             ResponseEntity.ok("Scooter state updated successfully")
         } else {
             ResponseEntity.status(HttpStatus.NOT_FOUND).body("Scooter not found")
         }
     }
 
-    // Add this method to ScooterController.kt in maintenance_service
     @PutMapping("/use_scooter/{scooterId}/")
     fun useScooter(@PathVariable scooterId: String): ResponseEntity<String> {
-        return try {
-            val scooterOptional = scooterRepository.findById(scooterId)
-            logger.info("Scooter with id $scooterId found")
-            if (scooterOptional.isPresent) {
-                val scooter = scooterOptional.get()
-                if (scooter.state == "ready") {
-                    scooter.state = "in use"
-                    scooterRepository.save(scooter)
-                    logger.info("Scooter with id $scooterId is now in use")
-                    ResponseEntity.ok("Scooter is now in use")
-                } else {
-                    logger.warn("Scooter with id $scooterId is not available")
-                    ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Scooter is not available")
-                }
+        val scooter = scooterService.useScooter(scooterId)
+        return if (scooter != null) {
+            if (scooter.state == "in use") {
+                ResponseEntity.ok("Scooter is now in use")
             } else {
-                logger.warn("Scooter with id $scooterId not found")
-                ResponseEntity.status(HttpStatus.NOT_FOUND).body("Scooter not found")
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Scooter is not available")
             }
-        } catch (e: Exception) {
-            logger.error(
-                "An error occurred while trying to use scooter with id $scooterId: ${e.message}"
-            )
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred")
+        } else {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body("Scooter not found")
         }
     }
 }
